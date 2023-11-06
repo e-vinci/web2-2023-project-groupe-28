27dc15888566fb1029564778d955f75da2aa025d
EmilienD <emilien.dieryck@student.vinci.be>
2023-11-06 15:02:03 +0100

    profilPage Base creation

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,13 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
+import ProfilPage from '../Pages/ProfilPage';
 
 const routes = {
   '/': HomePage,
   '/game': GamePage,
   '/new': NewPage,
+  '/profil' : ProfilPage,
 };
 
 export default routes;
