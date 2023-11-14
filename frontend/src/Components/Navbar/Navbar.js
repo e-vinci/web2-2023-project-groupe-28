12b60240fe72fdfdee3eb947e98c52ef478a1ebc
boxvers145 <62158594+boxvers145@users.noreply.github.com>
2023-11-14 19:44:32 +0100

    add rank page logo in navbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
   const navbar = `
         <div class="navbar bg-base-100">
     <div class="navbar-start">
-      <button id="cursor-Delete" class="btn btn-ghost" href="#" data-uri="/rank">Rank</button>
+      <button id="cursor-Delete" class="btn btn-ghost" href="#" data-uri="/rank"><svg data-uri="/rank"  class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path data-uri="/rank" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/></svg></button>
       <button id="cursor-Delete" class="btn btn-ghost" href="#" data-uri="/rule">Rules</button>
     </div>
 
--- frontend/src/Components/Pages/RankPage.js
+++ frontend/src/Components/Pages/RankPage.js
@@ -4,7 +4,33 @@ const RankPage = () => {
     clearPage();
     const main = document.querySelector('main');
     const login = `
-    <h1>this is RankPage</h1>
+    <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          
+          <ol>
+            <li>Pomme</li>
+            <li>Banane</li>
+            <li>Cerise</li>
+            <li>Datte</li>
+            <li>Endive</li>
+            <li>Fraise</li>
+            <li>Grenade</li>
+            <li>Haricot</li>
+            <li>Ignames</li>
+            <li>Jujube</li>
+            <li>Kiwi</li>
+            <li>Litchi</li>
+            <li>Mangue</li>
+            <li>Nectarine</li>
+            <li>Orange</li>
+            <li>Poire</li>
+            <li>Quetsche</li>
+            <li>Raisin</li>
+            <li>Satsuma</li>
+            <li>Tomate</li>
+          </ol>
+        </div>
+    </div>
     `
     main.innerHTML = login;
 
