eaaf36f155c55a6f328a74a79d257d1e8b5a4e66
Arthi25 <arthur.stoffels25@gmail.com>
2023-12-05 10:28:10 +0100

    feat: finishing CSS of rulePage + removing wrong CSS files

--- frontend/src/Components/Pages/ProfilPage.js
+++ frontend/src/Components/Pages/ProfilPage.js
@@ -15,30 +15,30 @@ const ProfilPage = () =>{
                     <div class="row m-l-0 m-r-0">
                         <div class="col-sm-4 bg-img-profil user-profile">
                             <div class="card-block text-center">
-                                    <div class="img-profil">
-                                        <img src="https://img.icons8.com/bubbles/100/000000/user.png" class="img-radius" alt="User-Profile-Image">
-                                    </div>
+                                <div class="img-profil">
+                                    <img src="https://img.icons8.com/bubbles/100/000000/user.png" class="img-radius" alt="User-Profile-Image">
+                                </div>
                             </div>
-                            </div>
-                                <div class="col-sm-8">
-                                    <div class="card-block">
-                                        <h6 class="m-b-20 p-b-5 b-b-default title-info-user">Information</h6>
-                                            <div class="row">
-                                                <div class="col-sm-6">
-                                                    <p class="box-ecart title-info-user">Username</p>
-                                                    <h6 class="text-muted title-info-user"></h6>
-                                                    <p class="box-ecart title-info-user">Number of game played</p>
-                                                       <h6 class="text-muted title-info-user"></h6>
-                                                    <p class="box-ecart title-info-user">Rank</p>
-                                                    <h6 class="text-muted title-info-user"></h6>
-                                                    <p class="box-ecart title-info-user">Number of win game</p>
-                                                    <h6 class="text-muted title-info-user"></h6>
-                                                    <p class="box-ecart title-info-user">Number of loss game</p>
-                                                    <h6 class="text-muted title-info-user"></h6>
-                                                </div>
-                                            </div>
-                                            </div>
+                        </div>
+                        <div class="col-sm-8">
+                            <div class="card-block">
+                                <h6 class="m-b-20 p-b-5 b-b-default title-info-user">Information</h6>
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <p class="box-ecart title-info-user">Username</p>
+                                            <h6 class="text-muted title-info-user"></h6>
+                                            <p class="box-ecart title-info-user">Number of game played</p>
+                                            <h6 class="text-muted title-info-user"></h6>
+                                            <p class="box-ecart title-info-user">Rank</p>
+                                            <h6 class="text-muted title-info-user"></h6>
+                                            <p class="box-ecart title-info-user">Number of win game</p>
+                                            <h6 class="text-muted title-info-user"></h6>
+                                            <p class="box-ecart title-info-user">Number of loss game</p>
+                                            <h6 class="text-muted title-info-user"></h6>
                                         </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- frontend/src/Components/Pages/RankPage.js
+++ frontend/src/Components/Pages/RankPage.js
@@ -4,11 +4,19 @@ const RankPage = () => {
     clearPage();
     const main = document.querySelector('main');
     const login = `
-    <h1>this is RankPage</h1>
+    
+    <div id="page-container" class="card w-72 mx-auto my-8 bg-base-100 text-primary-content p-4" >
+        <div id="padding">
+            <div id="card-body" class="card-body">
+                <h1 class="text-align-center">Rank</h1> 
+            </div>
+        </div>
+    </div>
+  
     `
     main.innerHTML = login;
 
 
   };
   
-  export default RankPage;
+export default RankPage;
