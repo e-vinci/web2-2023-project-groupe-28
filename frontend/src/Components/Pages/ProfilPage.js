19c0cc01c04103795d9d02001d44a83d0035e6f1
EmilienD <emilien.dieryck@student.vinci.be>
2023-12-11 15:24:34 +0100

    profilPage Frontend review

--- frontend/src/Components/Pages/ProfilPage.js
+++ frontend/src/Components/Pages/ProfilPage.js
@@ -49,9 +49,22 @@ function renderProfilPageForm() {
     span1.className = 'label-text';
     span1.innerText = 'Email';
 
+    const label1Email = document.createElement('label');
+    label1Email.className = 'label' 
+
     const span1Email = document.createElement('span');
     span1Email.className = 'label-text'
     span1Email.innerText = `${authenticatedUser.email}`
+
+    const div6 = document.createElement('div');
+    div6.className = 'form-control mt-6';
+
+    const submitEmail = document.createElement('input');
+    submitEmail.value = 'Change Email';
+    submitEmail.type = 'submit';
+    submitEmail.className = 'btn btn-outline';
+    submitEmail.id = 'neonButton';
+    submitEmail.setAttribute('data-theme', 'luxury');
 
     const label2 = document.createElement('label');
     label2.className = 'label';
@@ -60,28 +73,61 @@ function renderProfilPageForm() {
     span2.className = 'label-text';
     span2.innerText = 'Username';
 
-    const div5 = document.createElement('div');
-    div5.className = 'form-control';
+    const label2Username = document.createElement('label');
+    label2Username.className = 'label';
+
+    const span2Username = document.createElement('span');
+    span2Username.className = 'label-text';
+    span2Username.innerText = `${authenticatedUser.username}`;
+
+    const submitUsername = document.createElement('input');
+    submitUsername.value = 'Change Username';
+    submitUsername.type = 'submit';
+    submitUsername.className = 'btn btn-outline';
+    submitUsername.id = 'neonButton';
+    submitUsername.setAttribute('data-theme', 'luxury');
 
     const label3 = document.createElement('label');
     label3.className = 'label';
 
     const span3 = document.createElement('span');
     span3.className ='label-text';
-    span3.innerText = 'Password';
+    span3.innerText = 'score of the user';
+
+    const label3Score = document.createElement('label');
+    label3Score.className = 'label';
+
+    const span3Score = document.createElement('span');
+    span3Score.className = 'label-text';
+    span3Score.innerText = `${authenticatedUser.score}`;
 
     const label4 = document.createElement('label');
     label4.className = 'label';
 
-    const div6 = document.createElement('div');
-    div6.className = 'form-control mt-6';
+    const span4 = document.createElement('span');
+    span4.className = 'label-text';
+    span4.innerText = 'Number of loss games';
 
-    const submit = document.createElement('input');
-    submit.className = 'btn btn-outline';
-    submit.value = 'Register';
-    submit.type = 'submit';
-    submit.id = 'neonButton';
-    submit.setAttribute('data-theme', 'luxury');
+    const label4Loss = document.createElement('label');
+    label4Loss.className = 'label';
+
+    const span4Loss = document.createElement('span');
+    span4Loss.className = 'label-text';
+    span4Loss.innerText = `${authenticatedUser.defeatNumber}`;
+
+    const label5 = document.createElement('label');
+    label5.className = 'label';
+
+    const span5 = document.createElement('span');
+    span5.className = 'label-text';
+    span5.innerText = 'Number of win games';
+
+    const label5Win = document.createElement('label');
+    label5Win.className = 'label';
+
+    const span5Win = document.createElement('span');
+    span5Win.className = 'label-text';
+    span5Win.innerText = `${authenticatedUser.victoryNumber}`;
 
     main.appendChild(div1);
     main.appendChild(returnBtn);
@@ -92,17 +138,44 @@ function renderProfilPageForm() {
     div4.appendChild(title);
     div4.appendChild(label1);
     label1.appendChild(span1);
+    div4.appendChild(label1Email);
+    form.appendChild(div6);
+    div6.appendChild(submitUsername);
+    form.addEventListener('submit', changeEmail);
+    label1Email.appendChild(span1Email);
     div4.appendChild(label2);
     label2.appendChild(span2);
-    form.appendChild(div5);
-    div5.appendChild(label3);
-    label3.appendChild(span3)
-    div5.appendChild(label4);
+    div4.appendChild(label2Username);
+    label2Username.appendChild(span2Username);
     form.appendChild(div6);
-    div6.appendChild(submit);
-
+    div6.appendChild(submitUsername);
+    form.addEventListener('submit', changeUsername);
+    div4.appendChild(label3);
+    label3.appendChild(span3);
+    div4.appendChild(label3Score);
+    label3Score.appendChild(span3Score);
+    div4.appendChild(label4);
+    label4.appendChild(span4);
+    div4.appendChild(label4Loss);
+    label4Loss.appendChild(span4Loss); 
+    div4.appendChild(label5);
+    label5.appendChild(span5);
+    div4.appendChild(label5Win);
+    label5Win.appendChild(span5Win);
     returnHomePage();
 
     grow();
 }
+
+// to modify
+async function changeEmail(event){
+    console.log('in change email');
+    return event.preventDefault();
+}
+
+async function changeUsername(event){
+    console.log('in change username');
+    return event.preventDefault();
+}
+
 export default ProfilPage;
