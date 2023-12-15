feac9919f4ad9ca263a25ddbaad1f04b54df4309
Nemuriciu2 <94646639+Nemuriciu2@users.noreply.github.com>
2023-12-15 11:28:40 +0100

    adapted leaderboard.js model to correspond to the change of the login

--- api/models/leaderboard.js
+++ api/models/leaderboard.js
@@ -3,7 +3,8 @@ const pocketBase = require('pocketbase/cjs');
 const pb = new pocketBase('https://battleships.hop.sh');
 
 async function victory(user){
-    let userStat = await pb.collection('leaderboard').getFirstListItem(`user.id="${user.id}"`);
+    let idUser = await pb.collection('users').getFirstListItem(`username="${user.username}"`);
+    let userStat = await pb.collection('leaderboard').getFirstListItem(`user="${idUser.id}"`);
 
     const record = await pb.collection('leaderboard').update(userStat.id, {
         score: userStat.score += 2,
@@ -13,7 +14,8 @@ async function victory(user){
 }
 
 async function defeat(user){
-    let userStat = await pb.collection('leaderboard').getFirstListItem(`user.id="${user.id}"`);
+    let idUser = await pb.collection('users').getFirstListItem(`username="${user.username}"`);
+    let userStat = await pb.collection('leaderboard').getFirstListItem(`user="${idUser.id}"`);
 
     const record = await pb.collection('leaderboard').update(userStat.id, {
         score: userStat.score == 0 ? userStat.score : userStat.score -= 1,
